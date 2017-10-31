a33615f5b16193759dc4287c0a9462e772961e62
Woich <minewoichbr@gmail.com>
2017-10-30 23:09:33 -0200

    Funfa pelo amor de deus

--- src/main/java/Pratica61.java
+++ src/main/java/Pratica61.java
@@ -7,6 +7,26 @@
  */
 public class Pratica61 {
     public static void main(String[] args) {
-        System.out.println("Olá, Java!");
+        Time time1 = new Time();
+        
+        time1.addJogador("Goleiro", new Jogador(11, "Fulano 1"));
+        time1.addJogador("Zagueiro", new Jogador(12, "Beltrano 1"));
+        time1.addJogador("Atacante", new Jogador(13, "Cicrano 1"));
+        time1.addJogador("Lateral", new Jogador(14, "José 1"));
+        
+        Time time2 = new Time();
+        
+        time2.addJogador("Lateral", new Jogador(21, "Fulano 2"));
+        time2.addJogador("Atacante", new Jogador(22, "Beltrano 2"));
+        time2.addJogador("Goleiro", new Jogador(23, "Cicrano 2"));
+        time2.addJogador("Zagueiro", new Jogador(24, "José 2"));
+        
+        Set<String> entradas1 = time1.getJogadores().keySet();
+        
+        System.out.println("Posição \tTime 1 \t\t\t Time 2");
+        
+        for(String entrada1: entradas1) {
+            System.out.println(entrada1+" \t "+time1.getJogadores().get(entrada1)+"     \t "+time2.getJogadores().get(entrada1));
+        }
     }
 }
